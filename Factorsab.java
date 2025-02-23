5d4742a58ece3cdb3f20c6c34710ef226cef9b1a
Deepthi9 <113372909+deepthimegha@users.noreply.github.com>
2025-02-23 13:59:35 +0530

    Add files via upload

---
+++ Factorsab.java
@@ -0,0 +1,18 @@
+impact java.util.Scanner;
+class evenodd
+{
+Public static void main (string[]args)
+Scanner.sc = newscanner (system.in);
+System.out.println("enter the number");
+System.out.println("enter value of m");
+int m=scanner.nextInt()
+System.out.println("Enter value of n");
+int n = scanner.nextInt()
+for (int i=m;i<=n;i++)
+if(i%2==0)
+system.out.println("Even number")
+}
+else 
+} (odd number);
+}
+}
